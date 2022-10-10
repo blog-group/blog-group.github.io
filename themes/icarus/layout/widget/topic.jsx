21316a0cc3819eab20352c8e40d7ca91e46f1ed8
hengboy <jnyuqy@gmail.com>
2022-10-10 17:12:34 +0800

    :art: 调整专题列表样式

--- themes/icarus/layout/widget/topic.jsx
+++ themes/icarus/layout/widget/topic.jsx
@@ -1,6 +1,8 @@
 import {Component} from "inferno";
 import {require} from "hexo-component-inferno/lib/core/view";
+
 const Hr = require('./common/hr')
+
 /**
  * 专题列表
  */
@@ -8,21 +10,27 @@ class Topic extends Component {
     render() {
         const {widget} = this.props;
         const {projects} = widget;
-        const projectElements =projects.map((project) =>
-            <div class="card-content" style="font-size: 14px;padding-top: 1rem;">
-                <ul class="project">
-                    <li>
-                        <div class="project-left">
-                            <img src={project.image} alt={project.title}/>
-                        </div>
-                        <div class="project-right">
-                            <a href={project.url} target="_blank">
-                                <p class="title">{project.title}</p>
-                                <p class="desc">{project.desc}</p>
-                            </a>
-                        </div>
-                    </li>
-                </ul>
+        const projectElements = projects.map((project, index) =>
+            <div>
+                <div class="card-content" style="font-size: 14px;">
+                    <ul class="project">
+                        <li>
+                            <div class="project-left">
+                                <img src={project.image} alt={project.title}/>
+                            </div>
+                            <div class="project-right">
+                                <a href={project.url} target="_blank">
+                                    <p class="title">{project.title}</p>
+                                    <p class="desc">{project.desc}</p>
+                                </a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                {/* 分割线*/}
+                {
+                    index == projects.length - 1 ? null : <p style="border:0.2px solid #eee;width:70%;margin:0 auto;"></p>
+                }
             </div>
         );
         return <div class="card widget">{<Hr title="热门专题"/>}{projectElements}</div>;
